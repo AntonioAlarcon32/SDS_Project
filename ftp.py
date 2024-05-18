471c9453ec8631b5f03d061ab83552415b3a08f7
Antonio Alarcon <antonioalarcn32@gmail.com>
2024-05-19 00:10:30 +0200

    Bruteforce banning working

--- ftp.py
+++ ftp.py
@@ -2,12 +2,21 @@ import logging
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
+import socket
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 failed_attempts = {}
+controller_address = "10.100.100.100"
+controller_port = 9999
+fail_threshold = 3
+
+def send_udp_packet(dst_ip, dst_port, message):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(message.encode('utf-8'), (dst_ip, dst_port))
+    sock.close()
 
 class MyHandler(FTPHandler):
 
@@ -19,6 +28,9 @@ class MyHandler(FTPHandler):
         print(f"FAILED FROM {self.remote_ip}")
         if remote_ip in failed_attempts:
             failed_attempts[remote_ip] += 1
+            if failed_attempts[remote_ip] > fail_threshold:
+                send_udp_packet(controller_address, controller_port, f"ban;{remote_ip}")
+                print("User banned, packet sent to the controller")
         else:
             failed_attempts[remote_ip] = 1
         print(f"Failed attempts from {remote_ip}: {failed_attempts[remote_ip]}")
