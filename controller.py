3cfd7ca5cce8e3b069e35b88f5ab1c5b468dbd33
Antonio Alarcon <antonioalarcn32@gmail.com>
2024-05-23 00:51:29 +0200

    Working???

--- controller.py
+++ controller.py
@@ -19,7 +19,11 @@ from ryu.controller import dpset
 from ryu.lib import snortlib
 from ryu.app import ofctl_rest
 from operator import attrgetter
+from ryu.lib import dpid as dpid_lib
+import socket
+import datetime
 import array
+
 
 class ProjectController(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
@@ -60,6 +64,9 @@ class ProjectController(app_manager.RyuApp):
         self.datapaths = {
 
         }
+
+        self.UDP_IP = "127.0.0.1"
+        self.UDP_PORT = 8094
 
         self.public_ip = "80.80.80.80"
         self.monitoring_ip = "10.0.2.1"
@@ -110,7 +117,6 @@ class ProjectController(app_manager.RyuApp):
         
         wsgi_app.registory['CustomController'] = self.data
 
-        
         self.monitor_thread = hub.spawn(self._monitor)
 
     @set_ev_cls(snortlib.EventAlert, MAIN_DISPATCHER)
@@ -124,17 +130,16 @@ class ProjectController(app_manager.RyuApp):
         packet_str = f"Alert From Snort: {alert_str}"
         self.send_udp_packet(self.monitoring_ip, packet_str.encode("utf-8"), parser, alert_datapath, ofproto)
         
-    @set_ev_cls(ofp_event.EventOFPStateChange, [MAIN_DISPATCHER, DEAD_DISPATCHER])
     def _state_change_handler(self, ev):
         datapath = ev.datapath
         if ev.state == MAIN_DISPATCHER:
             if datapath.id not in self.datapaths:
-                self.logger.info('Register datapath: %016x', datapath.id)
+                self.logger.debug('register datapath: %016x', datapath.id)
                 self.datapaths[datapath.id] = datapath
         elif ev.state == DEAD_DISPATCHER:
             if datapath.id in self.datapaths:
-                self.logger.info('Unregister datapath: %016x', datapath.id)
-                del self.datapaths[datapath.id] 
+                self.logger.debug('unregister datapath: %016x', datapath.id)
+                del self.datapaths[datapath.id]
     
     def _monitor(self):
         while True:
@@ -150,44 +155,43 @@ class ProjectController(app_manager.RyuApp):
         req = parser.OFPFlowStatsRequest(datapath)
         datapath.send_msg(req)
 
-        req = parser.OFPPortStatsRequest(datapath)
+        req = parser.OFPPortStatsRequest(datapath, 0, ofproto.OFPP_ANY)
         datapath.send_msg(req)
 
     @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
     def _flow_stats_reply_handler(self, ev):
+        FLOW_MSG = "flows,datapath=%x in-port=%x,eth-dst=\"%s\",out-port=%x,packets=%d,bytes=%d %d"
         body = ev.msg.body
+        self.logger.info('stats received: %016x', ev.msg.datapath.id)
 
-        self.logger.info('datapath         '
-                         'in-port  eth-dst           '
-                         'out-port packets  bytes')
-        self.logger.info('---------------- '
-                         '-------- ----------------- '
-                         '-------- -------- --------')
         for stat in sorted([flow for flow in body if flow.priority == 1],
-                           key=lambda flow: (flow.match.get('in_port', 0),  # Use get method
+                           key=lambda flow: (flow.match['in_port'],
                                              flow.match['eth_dst'])):
-            self.logger.info('%016x %8x %17s %8x %8d %8d',
-                             ev.msg.datapath.id,
-                             stat.match.get('in_port', 0),  # Use get method
-                             stat.match['eth_dst'],
+            timestamp = int(datetime.datetime.now().timestamp() * 1000000000)
+            msg = FLOW_MSG % (ev.msg.datapath.id,
+                             stat.match['in_port'], stat.match['eth_dst'],
                              stat.instructions[0].actions[0].port,
-                             stat.packet_count, stat.byte_count)
+                             stat.packet_count, stat.byte_count,
+                             timestamp)
+            self.logger.info(msg)
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            sock.sendto(msg.encode(), (self.UDP_IP, self.UDP_PORT))
             
     @set_ev_cls(ofp_event.EventOFPPortStatsReply, MAIN_DISPATCHER)
     def _port_stats_reply_handler(self, ev):
+        PORT_MSG = "ports,datapath=%x,port=%x rx-pkts=%d,rx-bytes=%d,rx-error=%d,tx-pkts=%d,tx-bytes=%d,tx-error=%d %d"
         body = ev.msg.body
+        self.logger.info('stats received: %016x', ev.msg.datapath.id)
 
-        self.logger.info('datapath         port     '
-                         'rx-pkts  rx-bytes rx-error '
-                         'tx-pkts  tx-bytes tx-error')
-        self.logger.info('---------------- -------- '
-                         '-------- -------- -------- '
-                         '-------- -------- --------')
         for stat in sorted(body, key=attrgetter('port_no')):
-            self.logger.info('%016x %8x %8d %8d %8d %8d %8d %8d',
-                             ev.msg.datapath.id, stat.port_no,
+            timestamp = int(datetime.datetime.now().timestamp() * 1000000000)
+            msg = PORT_MSG % (ev.msg.datapath.id, stat.port_no,
                              stat.rx_packets, stat.rx_bytes, stat.rx_errors,
-                             stat.tx_packets, stat.tx_bytes, stat.tx_errors)
+                             stat.tx_packets, stat.tx_bytes, stat.tx_errors,
+                             timestamp)
+            self.logger.info(msg)
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            sock.sendto(msg.encode(), (self.UDP_IP, self.UDP_PORT))
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
